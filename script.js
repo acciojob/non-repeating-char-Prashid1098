d8160e7be0829b92be0920efa8d8ab8cebf782c6
Prashid Pal <67044025+Prashid1098@users.noreply.github.com>
2023-07-25 18:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function first_Nrc(str)
+{
+	let check=[];
+	for(let i=0;i<str.length();i++)
+		{
+			check[str.charAt(i)-'a']++;
+		}
+	for(let i=0;i<check.length;i++)
+		{
+			if(check[i]==1)
+			{
+				return i+'a';
+			}
+		}
+	return null;
+}
+let str=prompt("Enter a String");
+let result=first_Nrc(str);
+alert(result);
